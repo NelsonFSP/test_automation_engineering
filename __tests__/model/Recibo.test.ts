e68970e59a28adb3fd295b9383f981537957ca75
NelsonFSP <nfspdev@gmail.com>
2026-02-05 22:01:57 -0300

    feat: primeiro commit

---
+++ __tests__/model/Recibo.test.ts
@@ -0,0 +1,16 @@
+import { Recibo } from "../../src/model/Recibo"
+
+describe("Recibo", () => {
+    test("criar com sucesso", async () => {
+        const recibo: Recibo = new Recibo();
+        expect(recibo.codigo.length).toBe(6);
+    });
+
+    test("gerar codigo com Math.random mockado", () => {
+        const mockRandom = jest.spyOn(Math, 'random').mockReturnValue(0.5);
+        const recibo: Recibo = new Recibo();
+        expect(recibo.codigo).toBe("549999");
+        mockRandom.mockRestore();
+    });
+
+});
